831b61ad21b9ecf4e579994c282b416cadb9097d
namngo <hnnngo@mum.edu>
2018-04-02 11:59:07 -0500

    Init

---
+++ Lesson01.1-hibernate-xml/src/main/java/edu/mum/cs544/l01/model/Person.java
@@ -0,0 +1,36 @@
+package edu.mum.cs544.l01.model;
+
+public class Person {
+
+	private int id;
+    private String firstname;
+    private String lastname;
+
+    public Person() {
+    }
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+    
+}
