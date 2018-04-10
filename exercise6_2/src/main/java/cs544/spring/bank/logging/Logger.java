0be99db1139852b6ee0e1c3655d024311905332b
namngo <hnnngo@mum.edu>
2018-04-10 16:45:42 -0500

    Created

---
+++ exercise6_2/src/main/java/cs544/spring/bank/logging/Logger.java
@@ -0,0 +1,8 @@
+package cs544.spring.bank.logging;
+
+public class Logger implements ILogger{
+
+	public void log(String logstring) {
+		java.util.logging.Logger.getLogger("BankLogger").info(logstring);		
+	}
+}
---
+++ exercise6_2/src/main/java/cs544/spring/bank/logging/LoggingAdvice.java
@@ -0,0 +1,39 @@
+package cs544.spring.bank.logging;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAdvice {
+	@Autowired
+	private ILogger logger;
+
+	@After("execution(* *..bank.dao.*.*(..))")
+	public void afterMethod(JoinPoint joinPoint) {
+		logger.log("Called method in bank.dao: method =" + joinPoint.getSignature().getName());
+	}
+	
+	@Around("execution(* *..bank.service.*.*(..))")
+	public Object invoke(ProceedingJoinPoint call) throws Throwable {
+		StopWatch sw = new StopWatch();
+		sw.start(call.getSignature().getName());
+		Object retVal = call.proceed();
+		sw.stop();
+		long totaltime = sw.getLastTaskTimeMillis();
+		logger.log(String.format("Time to execute method = %s, time = %s", call.getSignature().getName(), totaltime));
+		return retVal;
+	}
+	
+	@After("execution(* *..JMSSender.sendJMSMessage(..))")
+	public void afterJMSMessageSent(JoinPoint joinPoint) {
+		logger.log("JMS Message sent = " + joinPoint.getArgs()[0]);
+	}
+
+}
