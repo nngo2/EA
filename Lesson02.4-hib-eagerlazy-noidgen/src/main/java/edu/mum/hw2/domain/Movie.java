78509c14dc5050a53f535185de994a934f79c575
namngo <hnnngo@mum.edu>
2018-09-24 09:43:27 -0500

    Created

---
+++ Lesson02.4-hib-eagerlazy-noidgen/src/main/java/edu/mum/hw2/domain/Movie.java
@@ -0,0 +1,38 @@
+package edu.mum.hw2.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+	@Id
+	private long id;
+	private String title;
+	@ElementCollection
+	private List<Artist> artists = new ArrayList<Artist>();
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Artist> getArtists() {
+		return artists;
+	}
+
+}
